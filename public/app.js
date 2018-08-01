19ee9754df73f56d760082761dfc82d9a9b88d9f
Greg Rutherford <g.rutherford10@gmail.com>
2018-08-01 16:30:59 +0100

    beers show as list

--- public/app.js
+++ public/app.js
@@ -1,5 +1,31 @@
 var app = function(){
   const url = "https://api.punkapi.com/v2/beers";
+  makeRequest(url, requestComplete);
+}
+
+const makeRequest = function(url, callBack){
+  const request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.addEventListener('load', callBack);
+  request.send();
+}
+
+const requestComplete = function(){
+  if(this.status !== 200) return;
+  const jsonString = this.responseText;
+  beers = JSON.parse(jsonString);
+  populateSelect(beers);
+}
+
+const populateSelect = function(beers){
+  const ul = document.getElementById('beers-list');
+  const img = document.createElement('img');
+  beers.forEach(function(beer, index) {
+    let li = document.createElement('li')
+    li.innerText = beer.name
+    li.value = index
+    ul.appendChild(li)
+  });
 }
 
 window.addEventListener('load', app);
